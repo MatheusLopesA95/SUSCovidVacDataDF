f653c4fc771ca73301aa19178f861c1f679eea49
Matheus Lopes <matheuscidao1995@gmail.com>
2022-11-19 22:12:12 -0300

    Last charger to Data bank
    The last charger on Data Bank before the preliminar analisys.

--- Carga_DB_Covid/Carga_DB_Covid/Tabelas/TB_Recuperados.sql
+++ Carga_DB_Covid/Carga_DB_Covid/Tabelas/TB_Recuperados.sql
@@ -3,12 +3,10 @@
 	[Cod_Estado] NVARCHAR(2) NOT NULL , 
     [Cod_Munincipio] NVARCHAR(50) NULL, 
     [Cod_Regiao] NVARCHAR NULL, 
-    [Cod_Regiao_Saude] NVARCHAR(50) NULL, 
     [Recuperados] INT NOT NULL, 
     [Cod_Dia] NVARCHAR(50) NOT NULL, 
     CONSTRAINT [FK_TB_Recuperados_TB_Estado] FOREIGN KEY ([Cod_Estado]) REFERENCES [TB_Estado]([Cod_Estado]), 
     CONSTRAINT [FK_TB_Recuperados_TB_Munincipio] FOREIGN KEY ([Cod_Munincipio]) REFERENCES [TB_Munincipio]([Cod_Munincipio]), 
     CONSTRAINT [FK_TB_Recuperados_TB_Regiao] FOREIGN KEY ([Cod_Regiao]) REFERENCES [TB_Regiao]([Cod_Regiao]), 
-    CONSTRAINT [FK_TB_Recuperados_TB_Regiao_Saude] FOREIGN KEY ([Cod_Regiao_Saude]) REFERENCES [TB_Regiao_Saude]([Cod_Regiao_Saude]), 
     CONSTRAINT [FK_Rel_Recuperados_Dim_Tempo] FOREIGN KEY ([Cod_Dia]) REFERENCES [Dim_Tempo]([Cod_Dia])
 )
